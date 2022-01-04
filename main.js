8cce23b5662f34b2d2d7ebf8f8289ac8d0dc70c5
suhyeon21 <suhyeon.kan@gmail.com>
2022-01-04 15:14:09 +0900

    refactor: using event delegation

--- main.js
+++ main.js
@@ -11,35 +11,45 @@ function onAdd(event) {
   input.focus();
 }
 
+let id = 0; //UUID 쓰는게 좋긴 함. 지금은 간단하게.
+
 function createItem(text) {
   const li = document.createElement("li");
   li.setAttribute("class", "item_row");
-
-  const div = document.createElement("div");
-  div.setAttribute("class", "item");
-
-  const span = document.createElement("span");
-  span.setAttribute("class", "item_name");
-  span.innerText = text;
-
-  const button = document.createElement("button");
-  button.setAttribute("class", "remove_button");
-  button.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  button.addEventListener("click", () => {
-    ul.removeChild(li);
-    input.focus();
-  });
-
-  const divider = document.createElement("div");
-  divider.setAttribute("class", "items_divider");
-
-  div.appendChild(span);
-  div.appendChild(button);
-
-  li.appendChild(div);
-  li.appendChild(divider);
-
+  li.setAttribute("data-id", id);
+  li.innerHTML = ` 
+            <div class="item">
+              <span class="item_name">${text}</span>
+              <button class="item_delete">
+                <i class="fas fa-trash-alt" data-id=${id}></i>
+              </button>
+            </div>
+            <div class="items_divider"></div>`;
+  id++;
   return li;
+  // const div = document.createElement("div");
+  // div.setAttribute("class", "item");
+
+  // const span = document.createElement("span");
+  // span.setAttribute("class", "item_name");
+  // span.innerText = text;
+
+  // const button = document.createElement("button");
+  // button.setAttribute("class", "remove_button");
+  // button.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  // button.addEventListener("click", () => {
+  //   ul.removeChild(li);
+  //   input.focus();
+  // });
+
+  // const divider = document.createElement("div");
+  // divider.setAttribute("class", "items_divider");
+
+  // div.appendChild(span);
+  // div.appendChild(button);
+
+  // li.appendChild(div);
+  // li.appendChild(divider);
 }
 
 plus.addEventListener("click", onAdd);
@@ -47,5 +57,13 @@ plus.addEventListener("click", onAdd);
 input.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     onAdd();
+  }
+});
+
+ul.addEventListener("click", (event) => {
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
